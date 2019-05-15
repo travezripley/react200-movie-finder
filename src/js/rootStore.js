cbb3f5184b90228d37798f40724586cc79cc3b8f
Travis Ripley <theripleys@gmail.com>
2019-05-15 15:50:32 -0700

    renders, rootReducer, index, app

---
+++ src/js/rootStore.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import rootReducer from "./rootReducer"
+//import thunk from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootStore = createStore(
+    rootReducer, composeEnhancers(applyMiddleware(promiseMiddleware)));
+
+export default rootStore;
