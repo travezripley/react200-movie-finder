15e7d35a5b3c32d98a1f442f1c82daa7a863e701
Travis Ripley <theripleys@gmail.com>
2019-05-17 09:11:10 -0700

    pushed to heroku

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './rootReducer';
-import {createPromise} from 'redux-promise-middleware';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./rootReducer";
+import { createPromise } from "redux-promise-middleware";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootStore = createStore(rootReducer, composeEnhancers(applyMiddleware(createPromise())));
+const rootStore = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(createPromise()))
+);
 
 export default rootStore;
